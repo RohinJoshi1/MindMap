66bce225c0d473c500f3b1251e8d8297581a738f
Jeevika Kiran <jeevika.kiran@gmail.com>
2023-09-21 21:37:42 +0530

    navbar done

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,9 @@
-
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { UserAuth } from "../context/AuthContext";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const Navbar = () => {
   const { user, googleSignIn, logOut } = UserAuth();
@@ -31,41 +33,120 @@ const Navbar = () => {
     checkAuthentication();
   }, [user]);
 
+  const navigation = [
+    { name: "My Journal", href: "/", current: true },
+    { name: "Recommendations", href: "/about", current: false },
+    { name: "Profile", href: "/profile", current: false },
+  ];
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
   return (
-    <div className="h-20 w-full border-b-2 flex items-center justify-between p-2">
-      <ul className="flex">
-        <li className="p-2 cursor-pointer">
-          <Link href="/">Home</Link>
-        </li>
-        <li className="p-2 cursor-pointer">
-          <Link href="/about">About</Link>
-        </li>
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-20">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                  alt="Your Company"
+                />
+              </div>
 
-        {!user ? null : (
-          <li className="p-2 cursor-pointer">
-            <Link href="/profile">Profile</Link>
-          </li>
-        )}
-      </ul>
+              <div className="hidden sm:block">
+                <div className="flex space-x-4">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                        "rounded-md px-3 py-2 text-sm font-medium",
+                        item.name === "Profile" && !user ? "cursor-not-allowed" : ""
+                      )}
+                      aria-current={item.current ? "page" : undefined}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
 
-      {loading ? null : !user ? (
-        <ul className="flex">
-          <li onClick={handleSignIn} className="p-2 cursor-pointer">
-            Login
-          </li>
-          <li onClick={handleSignIn} className="p-2 cursor-pointer">
-            Sign up
-          </li>
-        </ul>
-      ) : (
-        <div>
-          <p>Welcome, {user.displayName}</p>
-          <p className="cursor-pointer" onClick={handleSignOut}>
-            Sign out
-          </p>
-        </div>
+              <div className="sm:hidden">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+
+              {/* Login and Sign-up buttons for desktop view */}
+              <div className="hidden sm:block space-x-4">
+                {user ? (
+                  <div className="flex items-center space-x-2">
+                    <p className="text-white text-sm">Welcome, {user.displayName}</p>
+                    <button onClick={handleSignOut} className="text-gray-300 hover:text-white border border-white rounded px-3 py-1">
+                      Sign out
+                    </button>
+                  </div>
+                ) : (
+                  <div className="flex space-x-4">
+                    <button onClick={handleSignIn} className="text-gray-300 hover:text-white">
+                      Login
+                    </button>
+                    <button onClick={handleSignIn} className="text-gray-300 hover:text-white">
+                      Sign up
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block px-3 py-2 rounded-md text-base font-medium",
+                    item.name === "Profile" && !user ? "cursor-not-allowed" : ""
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </a>
+              ))}
+
+              {/* Login and Sign-up buttons for mobile view */}
+              {!user ? (
+                <div className="mt-4">
+                  <button onClick={handleSignIn} className="w-full block px-4 py-2 text-sm text-center text-gray-300 hover:bg-gray-700 hover:text-white">
+                    Login
+                  </button>
+                  <button onClick={handleSignIn} className="w-full block px-4 py-2 text-sm text-center text-gray-300 hover:bg-gray-700 hover:text-white">
+                    Sign up
+                  </button>
+                </div>
+                ) : (
+                    <button onClick={handleSignOut} className="w-full block px-4 py-2 text-sm text-center text-gray-300 hover:bg-gray-700 hover:text-white">
+                      Sign out
+                    </button>
+              )}
+            </div>
+          </Disclosure.Panel>
+        </>
       )}
-    </div>
+    </Disclosure>
   );
 };
 
