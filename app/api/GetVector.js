500ad15f927538e1531bd9226f8e049abafc9f01
rohanaj2001 <rohanaj2001@gmail.com>
2023-09-22 03:00:53 +0530

    added reduceCategoriesAndFormat function

--- app/api/GetVector.js
+++ app/api/GetVector.js
@@ -8,9 +8,18 @@ async function query(data) {
 		}
 	);
 	const result = await response.json();
-	return result;
+	return reduceCategoriesAndFormat(result);
 }
+// query({ "inputs": "I like you. I love you" }).then((response) => {
+// 	console.log(JSON.stringify(response));
+// });
 
-query({"inputs": "I like you. I love you"}).then((response) => {
-	console.log(JSON.stringify(response));
-});
+function reduceCategoriesAndFormat(data) {
+	const firstFourEntries = data[0].slice(0, 4);
+	const formattedData = {};
+	firstFourEntries.forEach((entry) => {
+		formattedData[entry.label] = entry.score;
+	});
+
+	return formattedData;
+}
